e711518b2cebb4eb24c49ce8e260f117e6c6e975
Rose Black <o2bablackrose@gmail.com>
2022-12-19 19:15:29 -0500

    Second commit

--- Assets/Scripts/trivia.js
+++ Assets/Scripts/trivia.js
@@ -1,4 +1,5 @@
 var timerE1 = document.getElementById('countdown');
+var requestUrl = "https://opentdb.com/api_config.php";
 
 function countdown() {
     var timeLeft = 90;
@@ -14,4 +15,20 @@ function countdown() {
       }, 1000);
     }
 
+    function getApi() {
+    
+      fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          for (var i = 0; i < data.length; i++) {
+      //      var listItem = document.createElement("li");
+      //      listItem.textContent = data[i].html_url;
+      //      repoList.appendChild(listItem);
+            console.log(data);
+          }
+        });
+    }
     countdown();
+    getApi();
